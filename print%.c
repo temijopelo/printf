da3047f86edcd6d9da467381b48f8b2492230780
Martinez <write2martinez@gmail.com>
2023-11-17 17:09:31 -0800

    func that prints the %

--- print%.c
+++ print%.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * print_pc - prints a percent sign (%).
- *
+ * print_pc - prints the percent sign (%).
  * Return: int
  */
 
 int print_pc(void)
 {
-	_putchar(37);
+	char percent_sign = '%';
+
+	if (_putchar(percent_sign) == -1)
+	{
+		return (0);
+	}
+
 	return (1);
 }
