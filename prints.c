7902bfa5180434991d6f7f48ac598a38a17c6499
Martinez <write2martinez@gmail.com>
2023-11-17 17:23:49 -0800

    printf

--- prints.c
+++ prints.c
@@ -3,18 +3,29 @@
 /**
  * str_print - prints a string
  * @val: arg for str
- * Return: the length of the string
+ * Return: string's length
  */
 
 int str_print(va_list val)
 {
-	char *s = va_arg(val, char *);
+	char *s;
+	int i;
+	int len;
 
+	s = va_arg(val, char *);
 	if (s == NULL)
+	{
 		s = "(null)";
-
-	for (int i = 0; s[i] != '\0'; i++)
-		_putchar(s[i]);
-
-	return (_strlen(s));
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
 }
